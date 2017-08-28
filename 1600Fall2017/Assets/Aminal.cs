406b9db50af5fac72e3b8f6fc4850946f748687d
Unknown <10818725@my.uvu.edu>
2017-08-28 11:59:47 -0600

    In Class Changes using Classes

---
+++ 1600Fall2017/Assets/Aminal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aminal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		die();
+	}
+
+	void die () {
+		print(this + " Died");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
