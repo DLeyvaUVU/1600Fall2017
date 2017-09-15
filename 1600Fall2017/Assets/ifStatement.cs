158291f9469b7e7eaa45f0252f375f79da37c7d3
Unknown <10818725@my.uvu.edu>
2017-09-15 11:07:59 -0600

    In Class Else Clauses and Math Operators

--- 1600Fall2017/Assets/ifStatement.cs
+++ 1600Fall2017/Assets/ifStatement.cs
@@ -7,7 +7,7 @@ public class ifStatement : MonoBehaviour {
 	public int num1;
 	public int num2;
 	public int value;
-	public bool canPlay = true;
+	public bool canPlay;
 	public Text input;
 	private string password = "1337G@m3r";
 
@@ -22,12 +22,20 @@ public class ifStatement : MonoBehaviour {
 			print("Play");
 		}
 	}
+	
 	void Update()
 	{
 		if (input.text == password) {
 			print("GJ, nerd, you know the password. Are you happy now?");
+			canPlay = true;
 		} else {
 			print("git gud");
+			canPlay = false;
+		}
+		if (canPlay) {
+			print("Playing Game...");
+		} else {
+			print("git gud, play when you know the right password");
 		}
 	}
 }
---
+++ 1600Fall2017/Assets/mathOperators.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mathOperators : MonoBehaviour {
+
+	public float num1, num2, addResult, subResult, multResult, divideResult, remainderResult;
+	// Update is called once per frame
+	void Update () {
+		addResult = num1 + num2;
+		subResult = num1 - num2;
+		multResult = num1 * num2;
+		if (num2 != 0) {
+			divideResult = num1/num2;
+		} else {
+			print("Can't divide by Zero.");
+		}
+		remainderResult = num1 % num2;
+	}
+}
