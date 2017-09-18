ed24a162c4e5ee19ce3a0a57a88b0b03154867d7
Unknown <10818725@my.uvu.edu>
2017-09-18 11:51:13 -0600

    Deleted Scripts, Added Buttons and Weapon Switching
    Some things got ruined by the merge.

---
+++ 1600Fall2017/Assets/WeaponSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour {
+	public string[] weapons;//[]Makes an array
+
+	public void MyWeaponSwitch (int i){
+		switch (weapons[i]){
+			case "Missile":
+				print("Missile");
+				break;
+			case "Plasma":
+				print("Plasma");
+				break;
+			default:
+				print("You have unrecognized alien technology");
+				break;
+		}
+	}
+}
