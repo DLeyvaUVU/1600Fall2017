90d5d312424e6cfeed9205636b6c9b970faa6c3b
Damian Leyva <10818725@my.uvu.edu>
2017-09-20 11:33:09 -0600

    Looped Pickup

---
+++ 1600Fall2017/Assets/Loops.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+	public string[] pickups;
+	public bool canPlay = true;
+	void Start (){
+		foreach (var item in pickups) {
+			print("I found a " + item + " pickup!");
+		}
+	}
+	void Update()
+	{
+		if (canPlay) {
+			print("Playing Game.");
+		} else {
+			print("Game Over");
+		}
+	}
+}
