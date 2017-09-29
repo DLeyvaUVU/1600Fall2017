042bd0ea3024cc6e4dc49e53e3aae1ca9ed35f6d
Unknown <10818725@my.uvu.edu>
2017-09-29 11:56:14 -0600

    Import Package In Class

--- 1600Fall2017/Assets/mathOperators.cs
+++ 1600Fall2017/Assets/mathOperators.cs
@@ -2,18 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mathOperators : MonoBehaviour {
+public class MathOperators : MonoBehaviour {
 
-	public float num1, num2, addResult, subResult, multResult, divideResult, remainderResult;
+	// Use this for initialization
+	public float num1;
+	public float num2;
+
+	public float addResult;
+	public float subResult;
+	public float multResult;
+	public float divResult;
+	public float remainderResult;
+
 	// Update is called once per frame
 	void Update () {
 		addResult = num1 + num2;
 		subResult = num1 - num2;
 		multResult = num1 * num2;
-		if (num2 != 0) {
-			divideResult = num1/num2;
+		if(num1 != 0 && num2 != 0) {
+			divResult = num1 / num2;	
 		} else {
-			print("Can't divide by Zero.");
+			print("Can't Divide By 0.");
 		}
 		remainderResult = num1 % num2;
 	}
--- 1600Fall2017/Assets/runGame.cs
+++ 1600Fall2017/Assets/runGame.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class runGame : MonoBehaviour {
+
+public class RunGame : MonoBehaviour {
+
 	public Toggle toggle;
-	// Update is called once per frame
-	void Update () {
+    public float health = 100;
+
+    // Update is called once per frame
+    void Update () {
 		if (toggle.isOn)
 		{
 			print("Play Game");
 		} else {
-			print("Can't Play");
+			print("Can't Play.");
 		}
+
+		health -= 0.01f;
 	}
 }
