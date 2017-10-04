b17691595fa6ce5d353f88623648b31e8d16229f
Unknown <10818725@my.uvu.edu>
2017-10-04 11:54:21 -0600

    Scope and Access

---
+++ 1600Fall2017/Assets/ScopeAndAccess.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeAndAccess : MonoBehaviour {
+	private int health = 100;
+	public int score = 100;//public can't be used in a function only in the class
+	public int checkHealth () {
+		return health;
+	}
+	void Update()
+	{
+		int getEnemiesKilled () {
+			return 13;
+		}
+		float getScoreModifier () {
+			int a = checkHealth();
+			int b = getEnemiesKilled();
+			float c;
+			if (b>5) {
+				c=1.3f;
+			}
+			return c;
+		}
+		int getNewScore() {
+			int a = getEnemiesKilled();
+			int b = 
+			return b;
+		}//can only be used in this function 
+		score = getNewScore();
+
+	}
+}
