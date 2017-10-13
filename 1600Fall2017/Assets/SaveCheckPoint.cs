c946509e98ea33a9073e3d64e6974e2990b48b2f
Unknown <10818725@my.uvu.edu>
2017-10-13 12:02:08 -0600

    In Game Checkpoint Saving

---
+++ 1600Fall2017/Assets/SaveCheckPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveCheckPoint : MonoBehaviour {
+	public GameObject player;
+	Vector3 checkPoint;
+	string[] playerPrefsTitles = {"CheckPointX", "CheckPointY", "CheckPointZ"};
+	void Start()
+	{
+		for (int i = 0; i < playerPrefsTitles.Length; i++)
+		{
+			checkPoint[i] = PlayerPrefs.GetFloat(playerPrefsTitles[i], checkPoint[i]);
+		}
+		player.transform.position = checkPoint;
+	}
+	void OnTriggerEnter()
+	{
+		checkPoint = transform.position;
+		for (int i = 0; i < playerPrefsTitles.Length; i++)
+		{
+			PlayerPrefs.SetFloat(playerPrefsTitles[i], checkPoint[i]);
+		}
+	}
+}
