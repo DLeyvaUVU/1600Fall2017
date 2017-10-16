98f0244ebc3ade4751d0e02a3409d74a999fef60
Unknown <10818725@my.uvu.edu>
2017-10-16 12:00:38 -0600

    Practiced using character controllers

---
+++ Yumps3D/Assets/Scripts/CharControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharControl : MonoBehaviour {
+	public CharacterController characterController;
+	public float gravity = 9.81f;
+	public Vector3 moveVector3;
+	public float speed = 10;
+	public float jumpForce = 100;
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+		moveVector3.y -= gravity * Time.deltaTime;
+		if (characterController.isGrounded) {
+			moveVector3.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+			if (Input.GetKey(KeyCode.Space)) {
+				moveVector3.y += jumpForce*Time.deltaTime;
+			}
+		}
+		characterController.Move(moveVector3);
+	}
+}
