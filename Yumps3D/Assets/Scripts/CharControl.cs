f4046ad22108be8f4af31f78ecc116e46acde7c3
Unknown <10818725@my.uvu.edu>
2017-10-18 11:47:46 -0600

    Made a Camera Controller

--- Yumps3D/Assets/Scripts/CharControl.cs
+++ Yumps3D/Assets/Scripts/CharControl.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class CharControl : MonoBehaviour {
 	public CharacterController characterController;
-	public float gravity = 9.81f;
+	public float gravity = 2.5f;
 	public Vector3 moveVector3;
 	public float speed = 10;
-	public float jumpForce = 100;
+	public float jumpForce = 50;
 	void Start () {
 		
 	}
 
-	void FixedUpdate () {
+	void Update () {
 		moveVector3.y -= gravity * Time.deltaTime;
 		if (characterController.isGrounded) {
 			moveVector3.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-			if (Input.GetKey(KeyCode.Space)) {
-				moveVector3.y += jumpForce*Time.deltaTime;
+			if (Input.GetKeyDown("space")) {
+				moveVector3.y = jumpForce*Time.deltaTime;
 			}
 		}
 		characterController.Move(moveVector3);
