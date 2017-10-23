40293ccd9857ddfc2a69ef42c07096619976b9dc
Unknown <10818725@my.uvu.edu>
2017-10-23 15:25:19 -0600

    Made Health Bar change from green, to yellow, to red
    math is cool

--- Yumps3D/Assets/Scripts/UI/UIbar.cs
+++ Yumps3D/Assets/Scripts/UI/UIbar.cs
@@ -5,16 +5,27 @@ using UnityEngine.UI;
 
 public class UIbar : MonoBehaviour {
 	public Image bar;
+	public Color barColor;
 	public float barTime = 0.1f;
 
 	// Use this for initialization
 	void Start () {
 		StartCoroutine(UpdateBar());
 	}
+	void Update()
+	{
+		bar.color = barColor;
+	}
 	
 	IEnumerator UpdateBar () {
 		while (bar.fillAmount > 0) {
-			bar.fillAmount -= barTime; 
+			bar.fillAmount -= barTime/10;
+			if (bar.fillAmount > 0.5f) {
+				barColor.r = ((1-bar.fillAmount)*2)*0.839f;
+			} else {
+				barColor.r = 0.839f;
+				barColor.g = bar.fillAmount*2*0.839f;
+			}
 			yield return new WaitForSeconds(barTime);
 		}
 	}
