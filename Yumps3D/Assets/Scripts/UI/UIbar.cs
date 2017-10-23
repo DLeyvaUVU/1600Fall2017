a0eb0d9183c54321a506396eaf06de7de32593ec
Unknown <10818725@my.uvu.edu>
2017-10-23 11:55:39 -0600

    Created a health Bar

---
+++ Yumps3D/Assets/Scripts/UI/UIbar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIbar : MonoBehaviour {
+	public Image bar;
+	public float barTime = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(UpdateBar());
+	}
+	
+	IEnumerator UpdateBar () {
+		while (bar.fillAmount > 0) {
+			bar.fillAmount -= barTime; 
+			yield return new WaitForSeconds(barTime);
+		}
+	}
+}
